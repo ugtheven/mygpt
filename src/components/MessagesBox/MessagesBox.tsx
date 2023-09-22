47978fdb7df28fad884d9335764eca3850f66605
S-i-d-V <me@ugotheveny.dev>
2023-09-22 23:18:52 +0200

    ✨ Code is now formatted in messages

--- src/components/MessagesBox/MessagesBox.tsx
+++ src/components/MessagesBox/MessagesBox.tsx
@@ -1,7 +1,79 @@
+import React from "react";
 import "./MessagesBox.scss";
 import botAvatar from '../../assets/chatgpt.jpg'
 import userAvatar from '../../assets/user.png'
 import { Message } from "../../features/Chat/messagesSlice";
+import { CopyBlock, nord } from "react-code-blocks";
+
+function getAvatar(role: string) {
+  if (role === "system") {
+    return botAvatar
+  } else {
+    return userAvatar
+  }
+}
+
+function splitMarkdownInMessage(message: string) {
+  const splitedMessage: { paragraph: string, type: string }[] = [];
+  const lines = message.split('\n');
+
+  let isMarkdown = false;
+  let paragraph = '';
+  let language = '';
+
+  lines.forEach((line) => {
+    if (!isMarkdown && line.startsWith('```')) {
+      isMarkdown = true;
+      splitedMessage.push({ paragraph, type: 'text' })
+      paragraph = ''
+      language = line.slice(3);
+    }
+    else if (isMarkdown && line.startsWith('```')) {
+      isMarkdown = false;
+      splitedMessage.push({ paragraph, type: language })
+      paragraph = '';
+      language = '';
+    }
+    else if (!isMarkdown)
+      paragraph += line + '\n'
+    else if (isMarkdown)
+      paragraph += line + '\n'
+  })
+  if (paragraph !== '')
+    splitedMessage.push({ paragraph, type: 'text' })
+  return splitedMessage
+}
+
+interface MessageProps {
+  message: Message
+}
+function MessageBox({ message }: MessageProps) {
+  const splitedMessage: { paragraph: string, type: string }[] = splitMarkdownInMessage(message.content)
+
+  return (
+    <div className={`message ${message.role === "system" ? 'bot': ''}`}>
+      <img src={getAvatar(message.role)} height='32px' width='32px' />
+      <div className="text">
+        {splitedMessage.map((line, index) => (
+          <div key={index}>
+            {line.type !== 'text' &&
+              <CopyBlock
+                text={line.paragraph}
+                showLineNumbers={false}
+                codeBlock
+                language={line.type}
+                theme={nord}
+              />
+            }
+            {line.type === 'text' &&
+              <p>{line.paragraph}</p>
+            }
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 interface MessagesBoxProps {
   messages: Message[]
@@ -11,17 +83,7 @@ function MessagesBox({ messages }: MessagesBoxProps) {
   return (
     <div className="messageContainer">
       {messages.map((message, index) => (
-        <div className={`message ${message.role === "system" ? 'bot': ''}`} key={index}>
-          {message.role === "system" &&
-            <img src={botAvatar} height='32px' width='32px' />
-          }
-          {message.role === "user" &&
-            <img src={userAvatar} height='32px' width='32px' />
-          }
-          <div className="text">
-            {message.content}
-          </div>
-        </div>
+        <MessageBox message={message} key={index}/>
       ))}
     </div>
   );
