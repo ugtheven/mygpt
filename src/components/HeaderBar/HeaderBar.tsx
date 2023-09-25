716d669601fcc48ccdec65ef41636d7d2b320158
S-i-d-V <me@ugotheveny.dev>
2023-09-25 23:03:37 +0200

    ✨ Add Header

---
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,11 @@
+import './HeaderBar.scss'
+
+function HeaderBar() {
+  return (
+    <div className="headerContainer">
+        <label>Default (GPT-4)</label>
+      </div>
+  );
+}
+
+export default HeaderBar;
--- src/features/Chat/Chat.tsx
+++ src/features/Chat/Chat.tsx
@@ -1,3 +1,4 @@
+import HeaderBar from "../../components/HeaderBar/HeaderBar";
 import InputBar from "../../components/InputBar/InputBar";
 import MessagesBox from "../../components/MessagesBox/MessagesBox";
 import { useAppSelector } from "../../hooks";
@@ -8,6 +9,7 @@ function Chat() {
 
   return (
     <div className="chatContainer">
+      <HeaderBar />
       <MessagesBox messages={messages} />
       <InputBar messages={messages} />
     </div>
